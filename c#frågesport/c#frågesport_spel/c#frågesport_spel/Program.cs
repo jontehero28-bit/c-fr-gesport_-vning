408c24ac0c91cef691800d6768daec0b9e1e4794
Yuriy Petrov <yuriy.petrov@elev.ga.ntig.se>
2022-08-31 14:22:32 +0200

    initial commit

---
+++ c#frågesport/c#frågesport_spel/c#frågesport_spel/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+string name;
+
+Console.WriteLine("Välkommen till universums dummaste frågesport!");
+Console.WriteLine("Vad heter du då?");
+
+name = Console.ReadLine();
+
+Console.WriteLine($"Välkommen, {name} hoppas du vinner!");
+
+
+
+
+
+
+//-------------------------------------------------------------------
+
+
+
